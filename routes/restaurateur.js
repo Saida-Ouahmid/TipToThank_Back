a603b0b04d7f4e965611a0fa01ec03a5dd84ca89
Marie Garnaud <garnaudm.e@gmail.com>
2020-07-20 10:00:07 +0200

    Connexion

--- routes/restaurateur.js
+++ routes/restaurateur.js
@@ -7,14 +7,14 @@ const restaurateurController = require("../controllers/restaurateur");
 /* Routes Menu */
 router.get("/menu", restaurateurController.getMenu);
 router.post("/menu/add", restaurateurController.addMenu);
-router.put("/menu/edit", restaurateurController.editMenu);
+
 router.delete("/menu/delete", restaurateurController.deleteMenu);
 router.put("/dailymenu/add", restaurateurController.addDailyMenu);
 
 /* Routes Inscription */
 router.post("/inscription", restaurateurController.inscription);
 /*Système de paiement !! */
-var restaurateurController = require("../controllers/restaurateur");
+router.post("/login", restaurateurController.login);
 
 /**
  * APPEL DE ROUTES DE PROFIL
@@ -27,17 +27,16 @@ router.get("/profil", restaurateurController.getProfil);
 router.put("/profil/edit", restaurateurController.editProfil);
 
 /* Appel du router de Recupération du QRCODE */
-router.get("/profil/qrcode");
+/*router.get("/profil/qrcode");
 
-/* Appel du router pour le désabonnement */
+/* Appel du router pour le désabonnement 
 router.delete("/profil/unsubscribe");
-
 /**
  * APPEL DES ROUTES GESTION DE PERSONNEL
  */
 
-/* Appel du router validation d'affiliation*/
-router.post("/management/affiliation");
+/* Appel du router validation d'affiliation
+router.post("/management/affiliation");*/
 
 /* Appel du router pour récupérer la liste server */
 router.get("/management/waiter-list", restaurateurController.getWaiterList);
@@ -49,7 +48,7 @@ router.put("/management/waiter-delete", restaurateurController.deleteWaiter);
  * APPEL DES ROUTES CONNEXION
  */
 
-/*Appel du router pour la connexion */
-router.post("/connexion");
+/*Appel du router pour la connexion 
+router.post("/connexion");*/
 
 module.exports = router;
