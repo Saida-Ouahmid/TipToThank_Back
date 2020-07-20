7067b934980be4d138fa76c65a08d141ec92f055
Arnaud Puaud <puaud.arnaud@outlook.fr>
2020-07-20 08:57:18 +0200

    1 Commit Ajout gestion Personnel

--- model/Restaurateur.js
+++ model/Restaurateur.js
@@ -4,20 +4,25 @@ const menu = new mongoose.Schema({
   drink: [{ picture: String, label: String }],
   otherMenu: [{ picture: String, label: String }],
 });
-const restaurateurSchema = new mongoose.Schema({
-  restaurantName: String,
-  email: String,
-  password: String,
-  siret: String,
-  bossName: String,
-  adress: String,
-  location: { longitude: String, latitude: String },
-  phone: String,
-  serviceNumber: { noon: Boolean, evening: Boolean },
-  logo: String,
-  picture: [],
-  menu: [menu],
-  qrCode: String,
-});
+const restaurateurSchema = new mongoose.Schema(
+  {
+    restaurantName: String,
+    email: String,
+    password: String,
+    siret: String,
+    bossName: String,
+    adress: String,
+    location: { longitude: String, latitude: String },
+    phone: String,
+    serviceNumber: { noon: Boolean, evening: Boolean },
+    logo: String,
+    picture: [],
+    menu: [menu],
+    qrCode: String,
+  },
+  {
+    collection: "restaurateurs",
+  }
+);
 
 module.exports = mongoose.model("Restaurateur", restaurateurSchema);
--- routes/restaurateur.js
+++ routes/restaurateur.js
@@ -2,10 +2,42 @@
 
 var express = require("express");
 var router = express.Router();
+var restaurateurController = require("../controllers/restaurateur");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+/**
+ * APPEL DE ROUTES DE PROFIL
+ */
+
+/* Appel du router profil affichage */
+router.get("/profil", restaurateurController.getProfil);
+
+/* Appel du router profil affichage */
+router.put("/profil/edit", restaurateurController.editProfil);
+
+/* Appel du router de Recupération du QRCODE */
+router.get("/profil/qrcode");
+
+/* Appel du router pour le désabonnement */
+router.delete("/profil/unsubscribe");
+
+/**
+ * APPEL DES ROUTES GESTION DE PERSONNEL
+ */
+
+/* Appel du router validation d'affiliation*/
+router.post("/management/affiliation");
+
+/* Appel du router pour récupérer la liste server */
+router.get("/management/waiter-list", restaurateurController.getWaiterList);
+
+/* Appel du router pour la suppression des serveurs */
+router.put("/management/waiter-delete", restaurateurController.deleteWaiter);
+
+/**
+ * APPEL DES ROUTES CONNEXION
+ */
+
+/*Appel du router pour la connexion */
+router.post("/connexion");
 
 module.exports = router;
